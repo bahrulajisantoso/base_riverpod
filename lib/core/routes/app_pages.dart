3fdafb1e791784352278299560b64d2b930a94b8
aji <ajidev26@gmail.com>
2026-01-17 17:54:45 +0700

    refactor: tidy up code

--- lib/core/routes/app_pages.dart
+++ lib/core/routes/app_pages.dart
@@ -1,6 +1,6 @@
 import 'package:go_router/go_router.dart';
-import '../../features/main/views/main_page.dart';
 import 'app_routes.dart';
+import '../../features/main/views/main_page.dart';
 
 class AppPages {
   static final GoRouter routes = GoRouter(
