e8001acac1436f38a7179d77844f6c388a03ec92
aji <ajidev26@gmail.com>
2026-01-20 21:18:08 +0700

    refactor: change name file provider brick

--- bricks/feature/__brick__/lib/features/{{feature_name.snakeCase()}}/providers/{{feature_name.snakeCase()}}_providers.dart
+++ bricks/feature/__brick__/lib/features/{{feature_name.snakeCase()}}/providers/{{feature_name.snakeCase()}}_providers.dart
@@ -3,7 +3,7 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part '{{feature_name.snakeCase()}}_providers.g.dart';
 
 @riverpod
-class {{feature_name.pascalCase()}}Providers extends _${{feature_name.pascalCase()}}Providers {
+class {{feature_name.pascalCase()}}Provider extends _${{feature_name.pascalCase()}}Providers {
   @override
   build() {}
 }
