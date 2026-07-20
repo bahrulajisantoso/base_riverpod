cc64d0ba602311aba4c3a7d49481f937af3c0963
aji <ajidev26@gmail.com>
2026-07-20 18:42:53 +0700

    refactor: rename notifier provider to state provider in feature brick

---
+++ bricks/feature/__brick__/lib/features/{{feature_name.snakeCase()}}/providers/{{feature_name.snakeCase()}}_state.dart
@@ -0,0 +1,9 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part '{{feature_name.snakeCase()}}_state.g.dart';
+
+@riverpod
+class {{feature_name.pascalCase()}}State extends _${{feature_name.pascalCase()}}State {
+  @override
+  void build() {}
+}
